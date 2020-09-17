8ff4c32ea29eb7b0e34ed771b590f2b57d233dc5
Szczepan <s.szablej1@gmail.com>
2020-09-17 16:28:11 +0200

    :zap: Add Spinner view to Travel while data is fetching

--- src/pages/Travel.js
+++ src/pages/Travel.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ColumnTemplate from "../templates/ColumnTemplate";
 import TravelCard from "../components/molecules/TravelCard/TravelCard";
+import Spinner from "../components/atoms/Spinner/Spinner";
 
 const StyledHeader = styled.h2`
   @media (max-width: 760px) {
@@ -11,25 +12,34 @@ const StyledHeader = styled.h2`
 
 const Travel = () => {
   const [travelInfo, setTravelInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const getTravelData = async () => {
+      setIsLoading(true);
       await fetch("https://api.coronatracker.com/v1/travel-alert")
         .then((response) => response.json())
         .then((data) => setTravelInfo(data));
+      setIsLoading(false);
     };
     getTravelData();
   }, []);
+
   return (
     <ColumnTemplate>
       <StyledHeader>Travel Alert Information</StyledHeader>
-      {travelInfo.map(({ countryName, alertMessage, publishedDate }) => (
-        <TravelCard
-          key={countryName}
-          country={countryName}
-          message={alertMessage}
-          date={publishedDate}
-        />
-      ))}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        travelInfo.map(({ countryName, alertMessage, publishedDate }) => (
+          <TravelCard
+            key={countryName}
+            country={countryName}
+            message={alertMessage}
+            date={publishedDate}
+          />
+        ))
+      )}
     </ColumnTemplate>
   );
 };
